1299602d463d729f70f119514049050728d91bd2
Joe Auz <joseph.auz77@gmail.com>
2022-11-23 14:45:38 -0600

    finished M05 Lab

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+    <!-- <v-toolbar app>
+
+    </v-toolbar> -->
+
+		<v-main>
+			<OurMessages />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OurMessages from './components/OurMessages';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+	components: {
+		OurMessages,
+	},
+
+	data: () => ({
+		//
+	}),
+};
+</script>
